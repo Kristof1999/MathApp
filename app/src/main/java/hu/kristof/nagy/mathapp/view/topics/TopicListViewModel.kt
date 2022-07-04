020d1375aeb7f31a50e20bf1d4f6c04de9c790dd
Nagy Kristof <nagykristof958@gmail.com>
2022-07-04 14:59:00 +0200

    Added create method

--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListViewModel.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListViewModel.kt
@@ -1,10 +1,12 @@
 package hu.kristof.nagy.mathapp.view.topics
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.kristof.nagy.mathapp.data.Database
 import hu.kristof.nagy.mathapp.data.entity.Topic
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +14,10 @@ class TopicListViewModel @Inject constructor(
     private val db: Database
 ) : ViewModel() {
     val topics: Flow<Topic> = db.topicDao().loadTopics()
+
+    fun create(topic: Topic) {
+        viewModelScope.launch {
+            db.topicDao().create(topic)
+        }
+    }
 }
