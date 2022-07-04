c9e10e34659d7a091158b5a3b92f39cc567ac14e
Nagy Kristof <nagykristof958@gmail.com>
2022-07-04 14:56:06 +0200

    Initialized list

--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListFragment.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListFragment.kt
@@ -8,11 +8,43 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
 import hu.kristof.nagy.mathapp.R
+import hu.kristof.nagy.mathapp.data.entity.Topic
+import hu.kristof.nagy.mathapp.databinding.FragmentTopicListBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class TopicListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentTopicListBinding.inflate(inflater, container, false)
 
+        val listViewModel: TopicListViewModel by viewModels()
+        val listItemViewModel: TopicListItemViewModel by viewModels()
+
+        val adapter = TopicListRecyclerViewAdapter(TopicClickListener(
+            editListener = { topic -> listItemViewModel.edit(topic) },
+            deleteListener = { topic -> listItemViewModel.delete(topic) }
+        ))
+        lifecycleScope.launch {
+            val list = mutableListOf<Topic>()
+            listViewModel.topics.collectLatest { topic ->
+                list.add(topic)
+            }
+            adapter.submitList(list)
+        }
+        binding.topicList.adapter = adapter
+
+        return binding.root
     }
 }
