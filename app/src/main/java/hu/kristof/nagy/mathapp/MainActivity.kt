6ecc0476fef0af4e1e69a337dd7599b52a143cc4
Nagy Kristof <nagykristof958@gmail.com>
2022-07-04 13:30:08 +0200

    Added annotations for hilt

--- app/src/main/java/hu/kristof/nagy/mathapp/MainActivity.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/MainActivity.kt
@@ -2,10 +2,23 @@ package hu.kristof.nagy.mathapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.NavigationUI
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
