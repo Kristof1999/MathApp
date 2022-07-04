e502ae48c36665ab6452e995868e1602cd7e83fa
Nagy Kristof <nagykristof958@gmail.com>
2022-07-04 11:00:52 +0200

    Added initial view for topics.

---
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListFragment.kt
@@ -0,0 +1,18 @@
+package hu.kristof.nagy.mathapp.view.topics
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.kristof.nagy.mathapp.R
+
+class TopicListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
