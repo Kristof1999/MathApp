85a675c32fe7b76521905e462b90954411a3a4fc
Nagy Kristof <nagykristof958@gmail.com>
2022-07-04 14:15:16 +0200

    Updated recyclerview adapter

--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListRecyclerViewAdapter.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListRecyclerViewAdapter.kt
@@ -5,41 +5,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import hu.kristof.nagy.mathapp.R
+import hu.kristof.nagy.mathapp.data.entity.Topic
 import hu.kristof.nagy.mathapp.databinding.FragmentTopicListItemBinding
+import java.nio.file.Files.size
 
 class TopicListRecyclerViewAdapter(
-    private val values: List<PlaceholderItem>
-) : RecyclerView.Adapter<TopicListRecyclerViewAdapter.ViewHolder>() {
+    private val clickListener: TopicClickListener
+) : ListAdapter<Topic, TopicListRecyclerViewAdapter.ViewHolder>(TopicListDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        return ViewHolder(
-            FragmentTopicListItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
-
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder.from(parent, clickListener)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        val topic = getItem(position)
+        holder.bind(topic)
     }
 
-    override fun getItemCount(): Int = values.size
+    class ViewHolder private constructor(
+        private val binding: FragmentTopicListItemBinding,
+        private val clickListener: TopicClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(topic: Topic) {
+            binding.topic = topic
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
 
-    inner class ViewHolder(binding: FragmentTopicListItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+        companion object {
+            fun from(parent: ViewGroup, clickListener: TopicClickListener): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = FragmentTopicListItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding, clickListener)
+            }
         }
     }
+}
 
+class TopicListDiffCallback : DiffUtil.ItemCallback<Topic>() {
+    override fun areItemsTheSame(oldItem: Topic, newItem: Topic): Boolean {
+        return oldItem.topicName == newItem.topicName
+    }
+
+    override fun areContentsTheSame(oldItem: Topic, newItem: Topic): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class TopicClickListener(
+    private val editListener: (topicName: String) -> Unit,
+    private val deleteListener: (topicName: String) -> Unit
+) {
+    fun onEdit(topicName: String) = editListener(topicName)
+    fun onDelete(topicName: String) = deleteListener(topicName)
 }
