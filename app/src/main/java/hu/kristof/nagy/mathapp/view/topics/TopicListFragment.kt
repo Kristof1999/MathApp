dad5bece25098dea3e174d7eb75d7c4cf655a6b3
Nagy Kristof <nagykristof958@gmail.com>
2022-07-05 14:51:49 +0200

    Updated flow collection and set lifecycle owner

--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListFragment.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/TopicListFragment.kt
@@ -1,25 +1,19 @@
 package hu.kristof.nagy.mathapp.view.topics
 
-import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import hu.kristof.nagy.mathapp.R
 import hu.kristof.nagy.mathapp.data.entity.Topic
 import hu.kristof.nagy.mathapp.databinding.FragmentTopicListBinding
 import hu.kristof.nagy.mathapp.view.TextDialogFragment
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -29,7 +23,11 @@ class TopicListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentTopicListBinding.inflate(inflater, container, false)
+        val binding = FragmentTopicListBinding.inflate(
+            inflater, container, false
+        ).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
 
         val listViewModel: TopicListViewModel by viewModels()
         val listItemViewModel: TopicListItemViewModel by viewModels()
@@ -65,13 +63,17 @@ class TopicListFragment : Fragment() {
             editListener = { topic -> listItemViewModel.edit(topic) },
             deleteListener = { topic -> listItemViewModel.delete(topic) }
         ))
+
         lifecycleScope.launch {
-            val list = mutableListOf<Topic>()
-            listViewModel.topics.collectLatest { topic ->
-                list.add(topic)
+            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                val list = mutableListOf<Topic>()
+                listViewModel.topics.collect { topic ->
+                    list.add(topic)
+                    adapter.submitList(list)
+                }
             }
-            adapter.submitList(list)
         }
+
         binding.topicList.adapter = adapter
     }
 }
