a78f1018b7ef5f6636a1c2debcc2e92e29822497
Nagy Kristof <nagykristof958@gmail.com>
2022-07-05 16:30:14 +0200

    Added exerciseDao

--- app/src/main/java/hu/kristof/nagy/mathapp/data/Database.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/Database.kt
@@ -2,10 +2,12 @@ package hu.kristof.nagy.mathapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import hu.kristof.nagy.mathapp.data.dao.ExerciseDao
 import hu.kristof.nagy.mathapp.data.dao.TopicDao
 import hu.kristof.nagy.mathapp.data.entity.Topic
 
 @Database(entities = [Topic::class], version = 1)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
+    abstract fun exerciseDao(): ExerciseDao
 }
