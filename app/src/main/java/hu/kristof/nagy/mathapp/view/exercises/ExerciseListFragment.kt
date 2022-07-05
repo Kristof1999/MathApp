08458093d73fa5a4b7caa3ab29c0d70fd03e4902
Nagy Kristof <nagykristof958@gmail.com>
2022-07-05 16:46:49 +0200

    Initialized list and exercise creation

--- app/src/main/java/hu/kristof/nagy/mathapp/view/exercises/ExerciseListFragment.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/exercises/ExerciseListFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import hu.kristof.nagy.mathapp.R
 import hu.kristof.nagy.mathapp.databinding.FragmentExerciseListBinding
+import hu.kristof.nagy.mathapp.view.TextDialogFragment
 
 class ExerciseListFragment : Fragment() {
     override fun onCreateView(
@@ -18,8 +21,65 @@ class ExerciseListFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
         }
 
+        val listViewModel: ExerciseListViewModel by viewModels()
+        val listItemViewModel: ExerciseListItemViewModel by viewModels()
 
+        initList(listItemViewModel, listViewModel, binding)
+
+        exerciseCreate(binding, listViewModel)
 
         return binding.root
+    }
+
+    private fun exerciseCreate(
+        binding: FragmentExerciseListBinding,
+        listViewModel: ExerciseListViewModel
+    ) {
+        binding.exerciseCreateBtn.setOnClickListener {
+            var name = ""
+            var question = ""
+            var answer = ""
+
+            val nameDialog = TextDialogFragment.instanceOf(
+                R.string.exerciseCreateNameText, R.string.exerciseCreateNameHint
+            )
+            nameDialog.show(parentFragmentManager, "exerciseCreateName")
+            nameDialog.text.observe(viewLifecycleOwner) {
+                name = it
+
+                val questionDialog = TextDialogFragment.instanceOf(
+                    R.string.exerciseCreateQuestionText, R.string.exerciseCreateQuestionHint
+                )
+                questionDialog.show(parentFragmentManager, "exerciseCreateQuestion")
+                questionDialog.text.observe(viewLifecycleOwner) {
+                    question = it
+
+                    val answerDialog = TextDialogFragment.instanceOf(
+                        R.string.exerciseCreateAnswerText, R.string.exerciseCreateAnswerHint
+                    )
+                    answerDialog.show(parentFragmentManager, "exerciseCreateAnswer")
+                    answerDialog.text.observe(viewLifecycleOwner) {
+                        answer = it
+
+                        listViewModel.create(name, question, answer)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initList(
+        listItemViewModel: ExerciseListItemViewModel,
+        listViewModel: ExerciseListViewModel,
+        binding: FragmentExerciseListBinding
+    ) {
+        val adapter = ExerciseListRecyclerViewAdapter(ExerciseClickListener(
+            editListener = { exercise -> listItemViewModel.edit(exercise) },
+            deleteListener = { exercise -> listItemViewModel.delete(exercise) }
+        ))
+        listViewModel.exercises.observe(viewLifecycleOwner) { exercises ->
+            adapter.submitList(exercises)
+        }
+        binding.exerciseList.adapter = adapter
     }
 }
