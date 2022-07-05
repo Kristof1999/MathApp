dcb2c1399ac0c6f217894073e4f5413e45ac0477
Nagy Kristof <nagykristof958@gmail.com>
2022-07-05 17:43:39 +0200

    Added automigration

--- app/src/main/java/hu/kristof/nagy/mathapp/data/Database.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/Database.kt
@@ -1,5 +1,6 @@
 package hu.kristof.nagy.mathapp.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import hu.kristof.nagy.mathapp.data.dao.ExerciseDao
@@ -7,7 +8,11 @@ import hu.kristof.nagy.mathapp.data.dao.TopicDao
 import hu.kristof.nagy.mathapp.data.entity.Exercise
 import hu.kristof.nagy.mathapp.data.entity.Topic
 
-@Database(entities = [Topic::class, Exercise::class], version = 2)
+@Database(
+    entities = [Topic::class, Exercise::class],
+    version = 2,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
     abstract fun exerciseDao(): ExerciseDao
