6cd1eab26fdf8e8a3bce28f47ec922a8dbc06929
Nagy Kristof <nagykristof958@gmail.com>
2022-07-05 16:03:31 +0200

    Added model and dao methods for exercise

---
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/dao/ExerciseDao.kt
@@ -0,0 +1,21 @@
+package hu.kristof.nagy.mathapp.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import hu.kristof.nagy.mathapp.data.entity.Exercise
+
+@Dao
+interface ExerciseDao {
+    // TODO: load only the names, and have a method which loads a one with the given name
+    @Query("SELECT * FROM exercise")
+    fun loadAll(): LiveData<List<Exercise>>
+
+    @Insert
+    fun create(exercise: Exercise)
+
+    @Delete
+    fun delete(exercise: Exercise)
+
+    @Update
+    fun edit(exercise: Exercise)
+}
---
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/entity/Exercise.kt
@@ -0,0 +1,10 @@
+package hu.kristof.nagy.mathapp.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Exercise(
+    @PrimaryKey val name: String,
+    val question: String, val answer: String
+)
