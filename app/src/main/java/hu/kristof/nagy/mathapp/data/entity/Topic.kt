54724af426a2ba5ccf9424451e7f9d8f5d45f3b7
Nagy Kristof <nagykristof958@gmail.com>
2022-07-06 08:27:39 +0200

    Adde javadoc for topic

--- app/src/main/java/hu/kristof/nagy/mathapp/data/entity/Topic.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/entity/Topic.kt
@@ -3,6 +3,12 @@ package hu.kristof.nagy.mathapp.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * A Topic is a mathematical topic, for example: algebra, calculus, etc.
+ * Topics can contain zero or more topics and exercises. For example:
+ * differential equations has topics: ordinary diff. eq. and partial diff. eq., and
+ * might even have exercises where one should categorise a given diff. eq..
+ */
 @Entity
 data class Topic(
     @PrimaryKey val topicName: String,
