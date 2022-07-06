3a3aeb1782bf02b881d67bf692cd179618b82871
Nagy Kristof <nagykristof958@gmail.com>
2022-07-06 09:06:02 +0200

    Fixed queries

--- app/src/main/java/hu/kristof/nagy/mathapp/data/dao/TopicDao.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/data/dao/TopicDao.kt
@@ -6,10 +6,10 @@ import hu.kristof.nagy.mathapp.data.entity.Topic
 
 @Dao
 interface TopicDao {
-    @Query("SELECT * FROM topic WHERE topic.parentTopicName == NULL")
+    @Query("SELECT * FROM topic WHERE topic.parentTopicName IS NULL")
     fun loadHighLevelTopics(): LiveData<List<Topic>>
 
-    @Query("SELECT * FROM topic WHERE topic.parentTopicName = :topicName")
+    @Query("SELECT * FROM topic WHERE topic.parentTopicName IS :topicName")
     fun loadTopicsFor(topicName: String): LiveData<List<Topic>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
