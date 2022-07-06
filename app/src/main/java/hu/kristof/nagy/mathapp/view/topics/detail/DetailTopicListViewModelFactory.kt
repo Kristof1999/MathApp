df12180beec24a7dafa264f67fcd270356f854c8
Nagy Kristof <nagykristof958@gmail.com>
2022-07-06 11:37:35 +0200

    Changed viewModel creation

--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/detail/DetailTopicListViewModelFactory.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/detail/DetailTopicListViewModelFactory.kt
@@ -2,11 +2,15 @@ package hu.kristof.nagy.mathapp.view.topics.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import hu.kristof.nagy.mathapp.data.MathAppDatabase
 
-class DetailTopicListViewModelFactory(private val parentTopicName: String) : ViewModelProvider.Factory {
+class DetailTopicListViewModelFactory(
+    private val db: MathAppDatabase,
+    private val parentTopicName: String
+    ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailTopicListViewModel::class.java)) {
-            return DetailTopicListViewModel(parentTopicName) as T
+            return DetailTopicListViewModel(db, parentTopicName) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
