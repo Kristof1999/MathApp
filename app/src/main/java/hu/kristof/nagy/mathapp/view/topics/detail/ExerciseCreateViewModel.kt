76964c66ffe1c35b055d1f84a848601b34b837c0
Nagy Kristof <nagykristof958@gmail.com>
2022-07-07 11:13:36 +0200

    Moved exercise create method to new viewModel

---
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/detail/ExerciseCreateViewModel.kt
@@ -0,0 +1,21 @@
+package hu.kristof.nagy.mathapp.view.topics.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.kristof.nagy.mathapp.data.MathAppDatabase
+import hu.kristof.nagy.mathapp.data.entity.Exercise
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExerciseCreateViewModel @Inject constructor(
+    private val db: MathAppDatabase
+) : ViewModel() {
+    fun create(name: String, question: String, answer: String, parentTopicName: String) {
+        val exercise = Exercise(null, name, question, answer, parentTopicName)
+        viewModelScope.launch {
+            db.exerciseDao().create(exercise)
+        }
+    }
+}
--- app/src/main/java/hu/kristof/nagy/mathapp/view/topics/detail/ExerciseListViewModel.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/topics/detail/ExerciseListViewModel.kt
@@ -1,21 +1,11 @@
 package hu.kristof.nagy.mathapp.view.topics.detail
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import hu.kristof.nagy.mathapp.data.MathAppDatabase
-import hu.kristof.nagy.mathapp.data.entity.Exercise
-import kotlinx.coroutines.launch
 
 class ExerciseListViewModel(
     parentTopicName: String,
     private val db: MathAppDatabase
 ) : ViewModel() {
     val exercises = db.exerciseDao().loadAllForTopic(parentTopicName)
-
-    fun create(name: String, question: String, answer: String, parentTopicName: String) {
-        val exercise = Exercise(null, name, question, answer, parentTopicName)
-        viewModelScope.launch {
-            db.exerciseDao().create(exercise)
-        }
-    }
 }
