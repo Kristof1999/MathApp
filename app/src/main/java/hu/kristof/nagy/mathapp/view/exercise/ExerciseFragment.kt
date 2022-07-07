1fcfb9145f7562a2cf137a5c4ca3782e687730bf
Nagy Kristof <nagykristof958@gmail.com>
2022-07-07 10:43:38 +0200

    Added sample MathJax code

--- app/src/main/java/hu/kristof/nagy/mathapp/view/exercise/ExerciseFragment.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/exercise/ExerciseFragment.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
 import hu.kristof.nagy.mathapp.databinding.FragmentExerciseBinding
+
 
 class ExerciseFragment : Fragment() {
     override fun onCreateView(
@@ -20,7 +27,25 @@ class ExerciseFragment : Fragment() {
         val args: ExerciseFragmentArgs by navArgs()
         binding.exerciseName.text = args.exercise.name
 
-        binding.exerciseQuestion.loadUrl("https://www.google.com/")
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(requireContext()))
+            .build()
+        binding.exerciseQuestionWebView.webViewClient = object : WebViewClientCompat() {
+            @RequiresApi(21)
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(request.url)
+            }
+        }
+        binding.exerciseQuestionWebView.settings.apply {
+            allowFileAccess = false
+            allowContentAccess = false
+            javaScriptEnabled = true
+        }
+        binding.exerciseQuestionWebView
+            .loadUrl("https://appassets.androidplatform.net/assets/index.html")
 
         return binding.root
     }
