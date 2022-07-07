7cb79566941249cb52a4c26201886ea05b81f673
Nagy Kristof <nagykristof958@gmail.com>
2022-07-07 14:06:47 +0200

    Moved back the convert method

--- app/src/main/assets/exerciseCreate.js
+++ app/src/main/assets/exerciseCreate.js
@@ -16,3 +16,35 @@ function createExercise() {
     }
     ExerciseCreateInterface.createExercise(name, question, answer);
 }
+
+function convert(inputId, buttonId, outputId) {
+    var input = document.getElementById(inputId).value.trim();
+
+    var button = document.getElementById(buttonId);
+    button.disabled = true;
+
+    output = document.getElementById(outputId);
+    output.innerHTML = '';
+
+    //
+    //  Reset the tex labels (and automatic equation numbers, though there aren't any here).
+    //  Get the conversion options (metrics and display settings)
+    //  Convert the input to CommonHTML output and use a promise to wait for it to be ready
+    //    (in case an extension needs to be loaded dynamically).
+    //
+    MathJax.texReset();
+    MathJax.tex2chtmlPromise(input).then(function (node) {
+        //
+        //  The promise returns the typeset node, which we add to the output
+        //  Then update the document to include the adjusted CSS for the
+        //    content of the new equation.
+        //
+        output.appendChild(node);
+        MathJax.startup.document.clear();
+        MathJax.startup.document.updateDocument();
+    }).catch(function (err) {
+        ExerciseCreateInterface.showToast(err.message);
+    }).then(function () {
+        button.disabled = false;
+    });
+}
