3025e81260db18d4a3fd37840bfc883b76c1d1fd
Nagy Kristof <nagykristof958@gmail.com>
2022-07-08 11:41:14 +0200

    Deleted resetting of tex labels

--- app/src/main/assets/exerciseCreate.js
+++ app/src/main/assets/exerciseCreate.js
@@ -26,13 +26,6 @@ function convert(inputId, buttonId, outputId) {
     output = document.getElementById(outputId);
     output.innerHTML = '';
 
-    //
-    //  Reset the tex labels (and automatic equation numbers, though there aren't any here).
-    //  Get the conversion options (metrics and display settings)
-    //  Convert the input to CommonHTML output and use a promise to wait for it to be ready
-    //    (in case an extension needs to be loaded dynamically).
-    //
-    MathJax.texReset();
     MathJax.tex2chtmlPromise(input).then(function (node) {
         //
         //  The promise returns the typeset node, which we add to the output
