612121da30522b470dcba1c646588f2b169f6a06
Nagy Kristof <nagykristof958@gmail.com>
2022-07-11 14:51:35 +0200

    Started using indexOf instead of search

--- app/src/main/assets/exerciseCreate.js
+++ app/src/main/assets/exerciseCreate.js
@@ -26,14 +26,17 @@ function convert(inputId, buttonId, outputId) {
     output = document.getElementById(outputId);
     output.innerHTML = '';
 
-    let prevMathBlockStartIdx = 0;
-    let prevMathBlockEndIdx = 0;
+    let prevMathBlockStartIdx = -1;
+    let prevMathBlockEndIdx = -1;
     let mathBlockStartIdx = input.indexOf("$");
-    let mathBlockEndIdx = mathBlockStartIdx + input.substring(mathBlockStartIdx + 1).indexOf("$");
-    while (mathBlockEndIdx != prevMathBlockEndIdx) {
-        let prevNonMathBlock = input.substring(prevMathBlockEndIdx, mathBlockStartIdx + 1);
-        let mathBlock = input.substring(mathBlockStartIdx, mathBlockEndIdx + 1);
+    let mathBlockEndIdx = input.indexOf("$", mathBlockStartIdx + 1);
+    while (mathBlockEndIdx != -1) {
+        let prevNonMathBlock = input.substring(prevMathBlockEndIdx + 1, mathBlockStartIdx);
+        let mathBlock = input.substring(mathBlockStartIdx + 1, mathBlockEndIdx);
+        ExerciseCreateInterface.showToast(mathBlock);
 
+        // TODO: switch to recursive functions
+        // because typesetting takes time, and some can be faster than others
         MathJax.tex2chtmlPromise(mathBlock).then(function (node) {
             output.innerHTML += prevNonMathBlock;
             output.innerHTML += node.innerHTML;
@@ -45,8 +48,8 @@ function convert(inputId, buttonId, outputId) {
 
         prevMathBlockStartIdx = mathBlockStartIdx;
         prevMathBlockEndIdx = mathBlockEndIdx;
-        mathBlockStartIdx = prevMathBlockEndIdx + input.substring(prevMathBlockEndIdx + 1).search("$");
-        mathBlockEndIdx = mathBlockStartIdx + input.substring(mathBlockStartIdx + 1).search("$");
+        mathBlockStartIdx = input.indexOf("$", prevMathBlockEndIdx);
+        mathBlockEndIdx = input.indexOf("$", mathBlockStartIdx + 1);
     }
     let lastNonMathBlock = input.substring(prevMathBlockEndIdx + 1);
     output.innerHTML += lastNonMathBlock;
