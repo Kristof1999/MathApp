d3aa3c896a1b94e5f464d46ed946f0967a02063d
Nagy Kristof <nagykristof958@gmail.com>
2022-07-11 08:07:01 +0200

    Implemented add step

--- app/src/main/assets/exercise.js
+++ app/src/main/assets/exercise.js
@@ -19,14 +19,29 @@ function onLoad() {
     });
 }
 
-function addStep() {
-    // it should be a div with class="output"
+function addStep(input) {
+    let steps = document.getElementById("steps");
+    MathJax.tex2chtmlPromise(input).then(function (node) {
+            node.style.width = "80%";
+            node.style.marginTop = ".75em";
+            node.style.border = "1px solid grey";
+            node.style.height = "50px";
+            node.style.overflow = "auto";
+            node.style.padding = "5px";
+            node.style.margin = "auto";
+            steps.appendChild(node);
+            MathJax.startup.document.clear();
+            MathJax.startup.document.updateDocument();
+        }).catch(function (err) {
+            ExerciseInterface.showToast(err.message);
+        });
 }
 
 function selectStep(value) {
     // show dialog if needed
     // transform input/prev step, and add step
     ExerciseInterface.showToast(value);
+    addStep("1 = \\frac{x}{y}");
 }
 
 function checkAnswer() {
