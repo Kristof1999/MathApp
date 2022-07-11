f38abbb1ccc1eb20a72a6040660a9bcf8f239be1
Nagy Kristof <nagykristof958@gmail.com>
2022-07-11 08:24:08 +0200

    Started implementing selectStep

--- app/src/main/assets/exercise.js
+++ app/src/main/assets/exercise.js
@@ -22,26 +22,47 @@ function onLoad() {
 function addStep(input) {
     let steps = document.getElementById("steps");
     MathJax.tex2chtmlPromise(input).then(function (node) {
-            node.style.width = "80%";
-            node.style.marginTop = ".75em";
-            node.style.border = "1px solid grey";
-            node.style.height = "50px";
-            node.style.overflow = "auto";
-            node.style.padding = "5px";
-            node.style.margin = "auto";
-            steps.appendChild(node);
-            MathJax.startup.document.clear();
-            MathJax.startup.document.updateDocument();
-        }).catch(function (err) {
-            ExerciseInterface.showToast(err.message);
-        });
+        node.style.width = "80%";
+        node.style.marginTop = ".75em";
+        node.style.border = "1px solid grey";
+        node.style.height = "50px";
+        node.style.overflow = "auto";
+        node.style.padding = "5px";
+        node.style.margin = "auto";
+        steps.appendChild(node);
+        MathJax.startup.document.clear();
+        MathJax.startup.document.updateDocument();
+    }).catch(function (err) {
+        ExerciseInterface.showToast(err.message);
+    });
 }
 
-function selectStep(value) {
-    // show dialog if needed
+function selectStep(stepType) {
+    // show dialog if needed -> prompt("please enter...","defaultText"); -> can be null or empty string
     // transform input/prev step, and add step
-    ExerciseInterface.showToast(value);
-    addStep("1 = \\frac{x}{y}");
+    let steps = document.getElementById("steps");
+    var prev = steps.lastChild;
+    if (prev == null) {
+        prev = ExerciseInterface.getExerciseQuestion();
+    } else {
+        prev = prev.value;
+    }
+    var stepInput = "";
+    switch(stepType) {
+        case "leftOrder":
+            let sides = prev.split("=");
+            stepInput = sides[0] + "-(" + sides[1] + ")";
+    }
+    addStep(stepInput);
+    // also call ExerciseInterface?
+}
+
+function getAsNegative(value) {
+    if (value == "0") {
+        return value;
+    } else {
+        return "-(" + value + ")";
+    }
 }
 
 function checkAnswer() {
