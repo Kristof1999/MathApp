de06374496ab9c3b6844c0934434e0c9b06a5f4d
Nagy Kristof <nagykristof958@gmail.com>
2022-07-12 09:55:40 +0200

    Updated names

--- app/src/main/assets/exerciseEdit/exerciseEdit.js
+++ app/src/main/assets/exerciseEdit/exerciseEdit.js
@@ -1,20 +1,20 @@
 function saveEdit() {
     let name = document.getElementById("name").value.trim();
     if (name.length == 0) {
-        ExerciseCreateInterface.showToast("A feladat neve nem lehet üres!");
+        ExerciseEditInterface.showToast("A feladat neve nem lehet üres!");
         return;
     }
     let question = document.getElementById("questionInput").value.trim();
     if (question.length == 0) {
-        ExerciseCreateInterface.showToast("A kérdés nem lehet üres!");
+        ExerciseEditInterface.showToast("A kérdés nem lehet üres!");
         return;
     }
     let answer = document.getElementById("answerInput").value.trim();
     if (answer.length == 0) {
-        ExerciseCreateInterface.showToast("A válasz nem lehet üres!");
+        ExerciseEditInterface.showToast("A válasz nem lehet üres!");
         return;
     }
-    ExerciseCreateInterface.saveEdit(name, question, answer);
+    ExerciseEditInterface.saveEdit(name, question, answer);
 }
 
 async function convertHelper(mathBlock) {
@@ -53,37 +53,4 @@ async function convert(inputId, buttonId, outputId) {
     output.innerHTML += lastNonMathBlock;
 
     button.disabled = false;
-}
-
-async function onLoadHelper(mathBlock) {
-    return MathJax.tex2chtmlPromise(mathBlock);
-}
-
-async function onLoad() {
-    let input = ExerciseInterface.getExerciseQuestion();
-
-    output = document.getElementById("question");
-    output.innerHTML = '';
-
-    let prevMathBlockStartIdx = -1;
-    let prevMathBlockEndIdx = -1;
-    let mathBlockStartIdx = input.indexOf("$");
-    let mathBlockEndIdx = input.indexOf("$", mathBlockStartIdx + 1);
-    while (mathBlockEndIdx != -1) {
-        let prevNonMathBlock = input.substring(prevMathBlockEndIdx + 1, mathBlockStartIdx);
-        let mathBlock = input.substring(mathBlockStartIdx + 1, mathBlockEndIdx);
-
-        let node = await onLoadHelper(mathBlock);
-        output.innerHTML += prevNonMathBlock;
-        output.innerHTML += node.innerHTML;
-        MathJax.startup.document.clear();
-        MathJax.startup.document.updateDocument();
-
-        prevMathBlockStartIdx = mathBlockStartIdx;
-        prevMathBlockEndIdx = mathBlockEndIdx;
-        mathBlockStartIdx = input.indexOf("$", prevMathBlockEndIdx);
-        mathBlockEndIdx = input.indexOf("$", mathBlockStartIdx + 1);
-    }
-    let lastNonMathBlock = input.substring(prevMathBlockEndIdx + 1);
-    output.innerHTML += lastNonMathBlock;
 }
