43896cb93918b0a23de2f2df8d64e24d46e890c3
Nagy Kristof <nagykristof958@gmail.com>
2022-07-12 09:39:10 +0200

    Added new exercise edit viewmodel

---
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/exercise/ExerciseEditViewModel.kt
@@ -0,0 +1,21 @@
+package hu.kristof.nagy.mathapp.view.exercise
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.kristof.nagy.mathapp.data.MathAppDatabase
+import hu.kristof.nagy.mathapp.data.entity.Exercise
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExerciseEditViewModel @Inject constructor(
+    private val db: MathAppDatabase
+) : ViewModel() {
+    fun edit(exercise: Exercise, name: String, question: String, answer: String) {
+        val newExercise = Exercise(exercise.id, name, question, answer, exercise.parentTopicName);
+        viewModelScope.launch {
+            db.exerciseDao().edit(newExercise)
+        }
+    }
+}
