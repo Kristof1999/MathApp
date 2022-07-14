73cfee3738273b7f82f1599b6d8df19a5eb74939
Nagy Kristof <nagykristof958@gmail.com>
2022-07-14 17:00:43 +0200

    Added visitor with todo

---
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/StepVisitor.kt
@@ -0,0 +1,5 @@
+package hu.kristof.nagy.mathapp.view.step
+
+class StepVisitor : LatexGrammarBaseVisitor<Expression>() {
+    // TODO: override visit methods to return the proper Step model classes
+}
