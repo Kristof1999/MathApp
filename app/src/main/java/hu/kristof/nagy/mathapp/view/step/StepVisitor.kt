0c08d8f7638391c74f6b82907afef2f91650a413
Nagy Kristof <nagykristof958@gmail.com>
2022-07-15 09:22:57 +0200

    Overridden operand visitor

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/StepVisitor.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/StepVisitor.kt
@@ -1,5 +1,38 @@
 package hu.kristof.nagy.mathapp.view.step
 
+import android.util.Log
+
 class StepVisitor : LatexGrammarBaseVisitor<Expression>() {
     // TODO: override visit methods to return the proper Step model classes
+    override fun visitOperand(ctx: LatexGrammarParser.OperandContext?): Expression {
+        return ctx?.let { operandContext ->
+            val leafText = operandContext.OPERAND().text
+            Log.println(Log.INFO, "StepVisitor", leafText)
+
+            // Double?
+            if (leafText.matches(Regex("[0-9]+([.,][0-9]+)?"))) {
+                return Value(leafText.toDouble())
+            }
+            if (leafText.matches(Regex("[a-zA-Z]+]"))) {
+                return Variable(leafText)
+            }
+
+            return when(leafText) {
+                "\\infty" -> Infinity()
+                "e" -> EulersNumber()
+                "\\pi" -> Pi()
+
+                "\\alpha" -> Alpha()
+                "\\beta" -> Beta()
+                "\\gamma" -> Gamma()
+                "\\omega" -> Omega()
+                "\\epsilon" -> Epsilon()
+                "\\varepsilon" -> VarEpsilon()
+                "\\phi" -> Phi()
+                "\\varphi" -> VarPhi()
+
+                else -> throw IllegalArgumentException("Unkown operand: " + leafText)
+            }
+        } ?: throw IllegalStateException("Operand is null")
+    }
 }
