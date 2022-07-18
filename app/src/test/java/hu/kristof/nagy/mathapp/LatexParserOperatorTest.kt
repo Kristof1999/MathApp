7832a7e1b13e7ce4f984f13241dc02deb4985623
Nagy Kristof <nagykristof958@gmail.com>
2022-07-18 08:50:49 +0200

    Fixed dependencies

--- app/src/test/java/hu/kristof/nagy/mathapp/LatexParserOperatorTest.kt
+++ app/src/test/java/hu/kristof/nagy/mathapp/LatexParserOperatorTest.kt
@@ -1,6 +1,7 @@
 package hu.kristof.nagy.mathapp
 
-import hu.kristof.nagy.mathapp.view.step.*
+import hu.kristof.nagy.mathapp.view.step.LatexParser
+import hu.kristof.nagy.mathapp.view.step.model.*
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
