f0000f3c53186e9088657d765553d32bfb614333
Nagy Kristof <nagykristof958@gmail.com>
2022-07-18 10:49:07 +0200

    Changed step transform function declaration

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/Ordering.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/Ordering.kt
@@ -6,14 +6,14 @@ import hu.kristof.nagy.mathapp.view.step.model.Subtraction
 import hu.kristof.nagy.mathapp.view.step.model.Value
 
 object LeftOrder : StepTransformer {
-    override fun transform(step: Expression): Expression {
+    override fun transform(step: Expression, x: Expression?): Expression {
         val equation = step as Equation
         return Equation(Subtraction(equation.leftSide, equation.rightSide), Value(0))
     }
 }
 
 object RightOrder : StepTransformer {
-    override fun transform(step: Expression): Expression {
+    override fun transform(step: Expression, x: Expression?): Expression {
         val equation = step as Equation
         return Equation(Value(0), Subtraction(equation.rightSide, equation.leftSide))
     }
--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/StepTransformer.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/StepTransformer.kt
@@ -3,5 +3,5 @@ package hu.kristof.nagy.mathapp.view.step.transform
 import hu.kristof.nagy.mathapp.view.step.model.Expression
 
 interface StepTransformer {
-    fun transform(step: Expression): Expression
+    fun transform(step: Expression, x: Expression?): Expression
 }
