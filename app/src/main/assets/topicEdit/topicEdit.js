66508a75e4913b06728c7799298413345e377148
Nagy Kristof <nagykristof958@gmail.com>
2022-07-19 10:51:35 +0200

    Added save function

--- app/src/main/assets/topicEdit/topicEdit.js
+++ app/src/main/assets/topicEdit/topicEdit.js
@@ -1,7 +1,17 @@
 function onLoad() {
     let nameElement = document.getElementById("topicName");
-    nameElement.innerHTML = TopicEditInterface.getName();
+    nameElement.value = TopicEditInterface.getName();
 
     let summaryElement = document.getElementById("input");
-    summaryElement.innerHTML = TopicEditInterface.getSummary();
+    summaryElement.value = TopicEditInterface.getSummary();
+}
+
+function save() {
+    let name = document.getElementById("topicName").value.trim();
+    if (name.length == 0) {
+        TopicEditInterface.showToast("A téma neve nem lehet üres!")
+        return;
+    }
+    let summary = document.getElementById("input").value.trim();
+    TopicEditInterface.save(name, summary)
 }
