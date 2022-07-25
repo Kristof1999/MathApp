7ae3717d79a7d414d8d6d07ead0704c4d859c34f
Kristof1999 <nagykristof958@gmail.com>
2022-07-25 11:29:15 +0200

    Updated to myBundle

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/Ordering.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/Ordering.kt
@@ -5,15 +5,17 @@ import hu.kristof.nagy.mathapp.view.step.model.Expression
 import hu.kristof.nagy.mathapp.view.step.model.Subtraction
 import hu.kristof.nagy.mathapp.view.step.model.Value
 
-object LeftOrder : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object LeftOrder : StepTransformer<StepTransformer.MyBundle> {
+    override fun transform(myBundle: StepTransformer.MyBundle): Expression {
+        val step = myBundle.step
         val equation = step as Equation
         return Equation(Subtraction(equation.leftSide, equation.rightSide), Value(0))
     }
 }
 
-object RightOrder : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object RightOrder : StepTransformer<StepTransformer.MyBundle> {
+    override fun transform(myBundle: StepTransformer.MyBundle): Expression {
+        val step = myBundle.step
         val equation = step as Equation
         return Equation(Value(0), Subtraction(equation.rightSide, equation.leftSide))
     }
