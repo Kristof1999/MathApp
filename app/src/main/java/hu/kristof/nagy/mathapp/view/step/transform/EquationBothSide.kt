bdd7bb84e674a454fd92ae939b2f4d7b18f3fc71
Kristof1999 <nagykristof958@gmail.com>
2022-07-25 11:29:29 +0200

    Updated to singleVariableMyBundle

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/EquationBothSide.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/EquationBothSide.kt
@@ -2,30 +2,38 @@ package hu.kristof.nagy.mathapp.view.step.transform
 
 import hu.kristof.nagy.mathapp.view.step.model.*
 
-object AddBothSideByX : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object AddBothSideByX : StepTransformer<StepTransformer.SingleVariableMyBundle> {
+    override fun transform(myBundle: StepTransformer.SingleVariableMyBundle): Expression {
+        val step = myBundle.step
+        val x = myBundle.x
         val equation = step as Equation
-        return Equation(Addition(equation.leftSide, x!!), Addition(equation.rightSide, x))
+        return Equation(Addition(equation.leftSide, x), Addition(equation.rightSide, x))
     }
 }
 
-object SubtractBothSideByX : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object SubtractBothSideByX : StepTransformer<StepTransformer.SingleVariableMyBundle> {
+    override fun transform(myBundle: StepTransformer.SingleVariableMyBundle): Expression {
+        val step = myBundle.step
+        val x = myBundle.x
         val equation = step as Equation
-        return Equation(Subtraction(equation.leftSide, x!!), Subtraction(equation.rightSide, x))
+        return Equation(Subtraction(equation.leftSide, x), Subtraction(equation.rightSide, x))
     }
 }
 
-object MultiplyBothSideByX : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object MultiplyBothSideByX : StepTransformer<StepTransformer.SingleVariableMyBundle> {
+    override fun transform(myBundle: StepTransformer.SingleVariableMyBundle): Expression {
+        val step = myBundle.step
+        val x = myBundle.x
         val equation = step as Equation
-        return Equation(Multiplication(equation.leftSide, x!!), Multiplication(equation.rightSide, x))
+        return Equation(Multiplication(equation.leftSide, x), Multiplication(equation.rightSide, x))
     }
 }
 
-object DivideBothSideByX : StepTransformer {
-    override fun transform(step: Expression, x: Expression?): Expression {
+object DivideBothSideByX : StepTransformer<StepTransformer.SingleVariableMyBundle> {
+    override fun transform(myBundle: StepTransformer.SingleVariableMyBundle): Expression {
+        val step = myBundle.step
+        val x = myBundle.x
         val equation = step as Equation
-        return Equation(Division(equation.leftSide, x!!), Division(equation.rightSide, x))
+        return Equation(Division(equation.leftSide, x), Division(equation.rightSide, x))
     }
 }
