49dc243ab9a2a1e1bb9de1465a6de79cd59fb906
Kristof1999 <nagykristof958@gmail.com>
2022-07-25 09:41:18 +0200

    Added media player

--- app/src/main/java/hu/kristof/nagy/mathapp/MainActivity.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/MainActivity.kt
@@ -1,24 +1,36 @@
 package hu.kristof.nagy.mathapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio)
+        mediaPlayer?.start()
+        mediaPlayer?.isLooping = true
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.navHostFragment)
         return navController.navigateUp()
+    }
+
+    override fun onStop() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+        super.onStop()
     }
 }
