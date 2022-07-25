83ca3b83736235fb61807a42ff304fb238e1d1f7
Kristof1999 <nagykristof958@gmail.com>
2022-07-25 11:28:42 +0200

    Added custom bundle and type parameter

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/StepTransformer.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/transform/StepTransformer.kt
@@ -2,6 +2,15 @@ package hu.kristof.nagy.mathapp.view.step.transform
 
 import hu.kristof.nagy.mathapp.view.step.model.Expression
 
-interface StepTransformer {
-    fun transform(step: Expression, x: Expression?): Expression
+interface StepTransformer<T : StepTransformer.MyBundle> {
+    fun transform(myBundle: T): Expression
+
+    open class MyBundle(
+        val step: Expression
+    )
+
+    class SingleVariableMyBundle(
+        step: Expression,
+        val x: Expression
+    ) : MyBundle(step)
 }
