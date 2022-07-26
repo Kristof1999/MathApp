ba9772b7a62d5908e5dcdac0a24cbcf6fd1627aa
Kristof1999 <nagykristof958@gmail.com>
2022-07-26 14:42:21 +0200

    Added javadoc

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/Parenthesize.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/Parenthesize.kt
@@ -3,10 +3,13 @@ package hu.kristof.nagy.mathapp.view.step
 import hu.kristof.nagy.mathapp.view.step.model.Expression
 import hu.kristof.nagy.mathapp.view.step.model.Parentheses
 
-fun parenthesize(expr: Expression): Expression =
+/**
+ * Returns the given expression in simple parentheses
+ * if it wasn't already in simple parentheses.
+ */
+fun parenthesizeIfNeeded(expr: Expression): Expression =
     if (expr is Parentheses) {
         expr
     } else {
-        // TODO: use different type of parentheses for readability
         Parentheses(expr)
     }
