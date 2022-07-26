3c28625717b82d667e80a2771b1502c5367de8c6
Kristof1999 <nagykristof958@gmail.com>
2022-07-26 11:54:20 +0200

    Added notes

--- app/src/test/java/hu/kristof/nagy/mathapp/latexparser/LatexParserIntegralTest.kt
+++ app/src/test/java/hu/kristof/nagy/mathapp/latexparser/LatexParserIntegralTest.kt
@@ -34,8 +34,16 @@ class LatexParserIntegralTest {
     @Test
     fun testDoubleDefinite() {
         // TODO: debug
+        // KNOW:
+        // println -> expression is parsed as a DefiniteIntegral
+        // DoubleIndefinite passes the test -> \ii doesn't become \i, precedence shouldn't be a problem too
+        // antlr plugin test -> grammar is ok
+        // build log -> newest grammar is compiled
+        // for "\\iiint_{0}^{1}{x}", it doesn't recognize '\iii', and parses as Variable(name='nt')
+        // debugger -> LatexParser sees the input as "\iint_{0}^{1}{x}"
         val expression = LatexParser.parse("\\iint_{0}^{1}{x}")
 
+        println(expression)
         assertTrue(expression is DoubleDefiniteIntegral)
         assertEquals(DoubleDefiniteIntegral(Value(0), Value(1), Variable("x")), expression)
     }
