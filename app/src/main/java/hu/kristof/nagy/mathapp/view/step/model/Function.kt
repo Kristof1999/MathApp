ec98d9b2e9459bdbd1b796adf6b612d1e6110e76
Kristof1999 <nagykristof958@gmail.com>
2022-07-26 14:45:53 +0200

    Refactored constructor, toLatex. Updated toLatex.

--- app/src/main/java/hu/kristof/nagy/mathapp/view/step/model/Function.kt
+++ app/src/main/java/hu/kristof/nagy/mathapp/view/step/model/Function.kt
@@ -1,52 +1,50 @@
 package hu.kristof.nagy.mathapp.view.step.model
 
+import hu.kristof.nagy.mathapp.view.step.parenthesizeIfNeeded
+
 abstract class Function(
-    private val argument: Expression
+    val argument: Expression
 ) : Expression
 
 open class CustomFunction(
-    private val name: String,
-    private val argument: Expression
+    val name: String,
+    argument: Expression
 ) : Function(argument) {
-    override fun toLatex(): String {
-        return """\operatorname{${name}}${Parentheses(argument)}"""
-    }
+    override fun toLatex(): String =
+        """\operatorname{${name}}${parenthesizeIfNeeded(argument)}"""
 }
 
-data class Sin(
-    private val argument: Expression
-) : CustomFunction("""\\sin""", argument)
+class Sin(argument: Expression) : Function(argument) {
+    override fun toLatex(): String = """\\sin${parenthesizeIfNeeded(argument)}"""
+}
 
-data class Cos(
-    private val argument: Expression
-) : CustomFunction("""\\cos""", argument)
+class Cos(argument: Expression) : Function(argument) {
+    override fun toLatex(): String = """\\cos${parenthesizeIfNeeded(argument)}"""
+}
 
-data class Limit(
-    private val variable: Variable,
-    private val argument: Expression,
-    private val limes: Value
+class Limit(
+    val variable: Variable,
+    argument: Expression,
+    val limes: Value
 ) : Function(argument) {
-    override fun toLatex(): String {
-        return """\lim_{${variable.toLatex()} \\to ${limes.toLatex()}} ${argument.toLatex()}"""
-    }
+    override fun toLatex(): String =
+        """\lim_{${variable.toLatex()} \\to ${limes.toLatex()}}{${argument.toLatex()}}"""
 }
 
-data class Sum(
-    private val lowerLimit: Expression,
-    private val upperLimit: Expression,
-    private val variable: Expression
-) : Expression {
-    override fun toLatex(): String {
-        return """\\sum_{${lowerLimit.toLatex()}}^{${upperLimit.toLatex()}} ${variable.toLatex()}"""
-    }
+class Sum(
+    val lowerLimit: Expression,
+    val upperLimit: Expression,
+    argument: Expression
+) : Function(argument) {
+    override fun toLatex(): String =
+        """\\sum_{${lowerLimit.toLatex()}}^{${upperLimit.toLatex()}}{${argument.toLatex()}}"""
 }
 
-data class Product(
-    private val lowerLimit: Expression,
-    private val upperLimit: Expression,
-    private val variable: Expression
-) : Expression {
-    override fun toLatex(): String {
-        return """\\prod_{${lowerLimit.toLatex()}}^{${upperLimit.toLatex()}} ${variable.toLatex()}"""
-    }
+class Product(
+    val lowerLimit: Expression,
+    val upperLimit: Expression,
+    argument: Expression
+) : Function(argument) {
+    override fun toLatex(): String =
+        """\\prod_{${lowerLimit.toLatex()}}^{${upperLimit.toLatex()}}{${argument.toLatex()}}"""
 }
