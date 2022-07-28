50d674463d3a90bec79a348234438eef48065ac9
Kristof1999 <nagykristof958@gmail.com>
2022-07-28 09:49:57 +0200

    Fixed expected expression for assertEquals

--- app/src/test/java/hu/kristof/nagy/mathapp/latexparser/FunctionTest.kt
+++ app/src/test/java/hu/kristof/nagy/mathapp/latexparser/FunctionTest.kt
@@ -39,7 +39,7 @@ class FunctionTest {
         val expression = LatexParser.parse("\\sum_{0}^{1}{x}")
 
         assertTrue(expression is Sum)
-        assertEquals(Sum(Value(0), Value(0), Variable("x")), expression)
+        assertEquals(Sum(Value(0), Value(1), Variable("x")), expression)
     }
 
     @Test
